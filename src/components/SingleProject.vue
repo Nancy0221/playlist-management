9945066404726e8707634d4f8ebae9db9d8fedb5
Nancy0221 <li.11901@osu.edu>
2025-05-02 17:51:38 -0400

    feat: mark the finished project

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,65 +1,81 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
-        <h3 @click="showDetails">{{ project.title }}</h3>
-        <div class="icons">
-            <span class="material-icons">edit</span>
-            <span @click="deleteProject" class="material-icons">delete</span>
-            <span class="material-icons">done</span>
-        </div>
+      <h3 @click="showDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-icons">edit</span>
+        <span @click="deleteProject" class="material-icons">delete</span>
+        <span @click="toggleComplete" class="material-icons tick">done</span>
+      </div>
     </div>
     <div v-if="showDetail" class="details">
-        <p>{{ project.details }}</p>
+      <p>{{ project.details }}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: [ 'project' ], 
-    data() {
-        return {
-            showDetail: false, 
-            url: 'http://localhost:3000/projects/' + this.project.id
-        }
-    }, 
-    methods: {
-        showDetails() {
-            this.showDetail = !this.showDetail
-        }, 
-        deleteProject() {
-            fetch(this.url, { method: 'DELETE' })
-                .then(() => this.$emit('delete', this.project.id))
-                .catch(err => console.log(err))
-        }
-    }
+  props: ["project"],
+  data() {
+    return {
+      showDetail: false,
+      url: "http://localhost:3000/projects/" + this.project.id,
+    };
+  },
+  methods: {
+    showDetails() {
+      this.showDetail = !this.showDetail;
+    },
+    deleteProject() {
+      fetch(this.url, { method: 'DELETE' })
+        .then(() => this.$emit('delete', this.project.id))
+        .catch((err) => console.log(err));
+    },
+    toggleComplete() {
+      fetch(this.url, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ complete: !this.project.complete }),
+      }).then(() => {
+        this.$emit('complete', this.project.id)
+      }).catch((err) => console.log(err));
+    },
+  },
 };
 </script>
 
 <style>
 .project {
-    margin: 20px auto;
-    background: white;
-    padding: 10px 20px;
-    border-radius: 4px;
-    box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
-    border-left: 4px solid #e90074;
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+  border-left: 4px solid #e90074;
+}
+h3 {
+  cursor: pointer;
+}
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.material-icons {
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+.material-icons:hover {
+  color: #777;
+}
+/* completed projects */
+  .project.complete {
+    border-left: 4px solid #00ce89;
   }
-  h3 {
-    cursor: pointer;
-  }
-  .actions {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .material-icons {
-    font-size: 24px;
-    margin-left: 10px;
-    color: #bbb;
-    cursor: pointer;
-  }
-  .material-icons:hover {
-    color: #777;
+  .project.complete .tick {
+    color: #00ce89;
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project" @delete="handleDelete" />
+        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
   </div>
@@ -24,14 +24,20 @@ export default {
     fetch('http://localhost:3000/projects')
       .then(res => res.json())
       .then(data => this.projects = data)
-      .catch(error => console.log(err.message))
+      .catch(err => console.log(err.message))
   }, 
   methods: {
     handleDelete(id) {
       this.projects = this.projects.filter((project) => {
         return project.id !== id
       })
-    }
+    }, 
+    handleComplete(id) {
+      let p = this.projects.find(project => {
+        return project.id === id
+      })
+      p.complete = !p.complete
+    } 
   }
 }
 </script>
