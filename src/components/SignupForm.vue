d0cb267b08b54b560fcfd89eaf50c3a0f92c2429
Nancy0221 <li.11901@osu.edu>
2025-05-08 01:43:52 -0400

    chore: update gitignore

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -20,7 +20,7 @@ import { ref } from "vue";
 import useSignup from "../composables/useSignup";
 
 export default {
-  setup() {
+  setup(props, context) {
     const { error, signup } = useSignup();
 
     // refs
@@ -30,7 +30,9 @@ export default {
 
     const handleSubmit = async () => {
       await signup(email.value, password.value, displayName.value);
-      console.log("user signed up");
+      if (!error.value) {
+        context.emit("signup");
+      }
     };
 
     return { displayName, email, password, handleSubmit, error };
