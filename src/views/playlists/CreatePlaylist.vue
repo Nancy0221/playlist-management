f9485063e0b6293d7d372dc9ad066496243e1cd5
Nancy0221 <li.11901@osu.edu>
2025-05-09 14:39:21 -0400

    feat: list view

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -12,23 +12,48 @@
     <input type="file" @change="handleChange" />
 
     <div class="error">{{ fileError }}</div>
-    <button>Create</button>
+    <button v-if="!isPending">Create</button>
+    <button v-else disabled>Saving...</button>
   </form>
 </template>
 
 <script>
 import { ref } from "vue";
+import useStorage from "@/composables/useStorage";
+import useCollection from "@/composables/useCollection";
+import getUser from "@/composables/getUser";
+import { timestamp } from "@/firebase/config";
 
 export default {
   setup() {
+    const { filePath, url, uploadImage } = useStorage();
+    const { error, addDoc } = useCollection("playlists");
+    const { user } = getUser();
+
     const title = ref("");
     const description = ref("");
     const file = ref(null);
     const fileError = ref(null);
+    const isPending = ref(false);
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
       if (file.value) {
-        console.log(title.value, description.value, file.value);
+        isPending.value = true;
+        await uploadImage(file.value);
+        await addDoc({
+          title: title.value,
+          description: description.value,
+          userId: user.value.uid,
+          userName: user.value.displayName,
+          coverUrl: "",
+          filePath: filePath.value,
+          songs: [],
+          createdAt: timestamp(),
+        });
+        isPending.value = false;
+        if (!error.value) {
+          console.log("playlist added");
+        }
       }
     };
 
@@ -48,7 +73,14 @@ export default {
       }
     };
 
-    return { title, description, handleSubmit, handleChange, fileError };
+    return {
+      title,
+      description,
+      handleSubmit,
+      handleChange,
+      fileError,
+      isPending,
+    };
   },
 };
 </script>
